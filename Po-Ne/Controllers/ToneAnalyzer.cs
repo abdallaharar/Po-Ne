7cab2f6addda2bb454267473dfa802397053da24
PragashSiva <pragash.acci@gmail.com>
2015-09-19 19:29:25 -0400

    NewtonSoft

--- Po-Ne/Controllers/ToneAnalyzer.cs
+++ Po-Ne/Controllers/ToneAnalyzer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 using System.Runtime.Serialization;
-using Json;
+using Newtonsoft.Json;
 
 namespace Watson
 {
@@ -19,8 +19,8 @@ namespace Watson
             using (var client = new WebClient())
             {
                 String userName = "f2f8a4ad-f87c-487a-85fe-92a8d150944e";
-                String passWord = "QUSjnSsOtgEU";
-                String credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(userName + ":" + passWord));
+                String password = "QUSjnSsOtgEU";
+                String credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(userName + ":" + password));
                 client.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
                 //"{\"scorecard\":\"test\",\"text\":\"I am sad\"}"
                 String json = "I am sad";
@@ -28,7 +28,7 @@ namespace Watson
                 result = client.UploadString("https://gateway.watsonplatform.net/tone-analyzer-experimental/api/v1/tone", json);
 
 
-                JsonObject resultJson = JsonParser.Deserialize(result);
+                var resultJson = JsonConvert.DeserializeObject(result);
                 Console.WriteLine(resultJson);
             }
         }
