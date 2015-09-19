6cf520c9f8e0e703ace7b5cf7a5bd5bc5c9d1cb7
PragashSiva <pragash.acci@gmail.com>
2015-09-19 16:59:32 -0400

    Added REST to Watson capabilities

---
+++ Po-Ne/Controllers/ToneAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Watson
+{
+    class ToneAnalyzer
+    {
+
+        public static void getToneAnalysis(String scorecard, String body)
+        {
+            string result = "";
+            using (var client = new WebClient())
+            {
+                String userName = "f2f8a4ad-f87c-487a-85fe-92a8d150944e";
+                String passWord = "QUSjnSsOtgEU";
+                String credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(userName + ":" + passWord));
+                client.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+                //"{\"scorecard\":\"test\",\"text\":\"I am sad\"}"
+                String json = "I am sad";
+                client.Headers[HttpRequestHeader.ContentType] = "text/plain";
+                result = client.UploadString("https://gateway.watsonplatform.net/tone-analyzer-experimental/api/v1/tone", json);
+                Console.WriteLine( JsonConvert.DeserializeObject(result));
+            }
+        }
+
+        public static void Main()
+        {
+            getToneAnalysis("test", "I am sad");
+        }
+
+    }
+}
