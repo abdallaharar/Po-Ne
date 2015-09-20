fef5bf3b7cbf9334ef29c9bc02d359ba23383be7
PragashSiva <pragash.acci@gmail.com>
2015-09-20 07:12:54 -0400

    test

--- Po-Ne/Controllers/HomeController.cs
+++ Po-Ne/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Po_Ne.Controllers
         public JsonResult RetrieveWatsonFeedback(string dataRequest)
         {
             Po_Ne.Controllers.Client twitter = new Po_Ne.Controllers.Client();
-            var variable = "?q=" + dataRequest + "&result_type=recent&lang=en";
+            var variable = "?q=" + dataRequest + "&result_type=popular&lang=en";
             ViewData["WatsonResult"] = twitter.GetSearchJson(twitter.GetBearerToken(), variable.ToString());
             var percentage = Po_Ne.Controllers.ToneAnalyzer.judge(ViewData["WatsonResult"].ToString());
             return Json(percentage);
